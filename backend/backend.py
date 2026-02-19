f208d715bf70c449e87273bcd048f1bf8e8ede82
Ayo <ayodyae822@gmail.com>
2026-02-19 16:51:46 +0530

    Add backend folder

---
+++ backend/backend.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Backend working"}
+
+@app.get("/add")
+def add(a: float, b: float):
+    return {"result": a + b}
+
+@app.get("/subtract")
+def subtract(a: float, b: float):
+    return {"result": a - b}
+
+@app.get("/multiply")
+def multiply(a: float, b: float):
+    return {"result": a * b}
+
+@app.get("/divide")
+def divide(a: float, b: float):
+    return {"result": a / b}
